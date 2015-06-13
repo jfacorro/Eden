29c75ade77e86fbb122f3ec0ca3a102e48fa7831
Juan Facorro <juan.facorro@gmail.com>
2015-06-13 14:08:59 -0300

    [#17] Published to hex

--- mix.exs
+++ mix.exs
@@ -5,30 +5,35 @@ defmodule ExEdn.Mixfile do
     [app: :ex_edn,
      version: "0.1.2",
      elixir: "~> 1.0",
+     description: description,
+     package: package,
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type `mix help compile.app` for more information
   def application do
     [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type `mix help deps` for more examples and options
   defp deps do
     [{:exreloader, github: "jfacorro/exreloader", tag: "master", only: :dev},
      {:array, "~> 1.0.1"},
      {:timex, "~> 0.13.4"}]
+  end
+
+  defp description do
+    """
+    [edn](https://github.com/edn-format/edn) (extensible data notation)
+    encoder/decoder implemented in Elixir.
+    """
+  end
+
+  defp package do
+    [files: ["lib", "priv", "mix.exs", "README*", "readme*", "LICENSE*", "license*"],
+     contributors: ["Juan Facorro"],
+     licenses: ["Apache 2.0"],
+     links: %{"GitHub" => "https://github.com/jfacorro/ExEdn/",
+              "Docs" => "http://jfacorro.github.io/ExEdn/"}]
   end
 end
