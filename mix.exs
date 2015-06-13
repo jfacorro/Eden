996ff4d2748ea7335e5c524c4bb0b3fac1719c6c
Juan Facorro <juan.facorro@gmail.com>
2015-06-13 17:28:27 -0300

    Version bump to 0.1.3

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Eden.Mixfile do
 
   def project do
     [app: :eden,
-     version: "0.1.2",
+     version: "0.1.3",
      elixir: "~> 1.0",
      description: description,
      package: package,
