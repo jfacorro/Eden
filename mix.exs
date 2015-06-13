6aa6640e1c6d21e0b8d357992472b0a1659937af
Juan Facorro <juan.facorro@gmail.com>
2015-06-13 13:48:42 -0300

    [#18] Version bump 0.1.2

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule ExEdn.Mixfile do
 
   def project do
     [app: :ex_edn,
-     version: "0.0.1",
+     version: "0.1.2",
      elixir: "~> 1.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
