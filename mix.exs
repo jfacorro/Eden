1135f4dad59312ded40be5b7f613567e12c1cc4f
Juan Facorro <juan.facorro@gmail.com>
2016-09-16 23:27:18 +0200

    Update Elixir version and bump version

--- mix.exs
+++ mix.exs
@@ -3,8 +3,8 @@ defmodule Eden.Mixfile do
 
   def project do
     [app: :eden,
-     version: "0.1.3",
-     elixir: "~> 1.0",
+     version: "1.0.0",
+     elixir: "~> 1.3.2",
      description: description,
      package: package,
      build_embedded: Mix.env == :prod,
@@ -21,8 +21,8 @@ defmodule Eden.Mixfile do
      {:timex, "~> 1.0.0"},
 
      {:exreloader, github: "jfacorro/exreloader", tag: "master", only: :dev},
-     {:ex_doc, "~> 0.7", only: :docs},
-     {:earmark, ">= 0.0.0", only: :docs}]
+     {:ex_doc, "~> 0.7", only: :dev},
+     {:earmark, ">= 0.0.0", only: :dev}]
   end
 
   defp description do
