93e875ebfc46f556dd9438e8862ef247e8b287a7
Juan Facorro <juan.facorro@klarna.com>
2020-10-18 17:21:19 +0200

    Replace Travis CI configuration and replace with GitHub actions

--- mix.exs
+++ mix.exs
@@ -5,11 +5,11 @@ defmodule Eden.Mixfile do
     [app: :eden,
      version: "2.1.0",
      elixir: "~> 1.11",
-     description: description,
-     package: package,
+     description: description(),
+     package: package(),
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps]
+     deps: deps()]
   end
 
   def application do
