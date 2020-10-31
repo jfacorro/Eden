9c5ec4300e3c8e119d63b3d796ba391612401404
Juan Facorro <juan.facorro@klarna.com>
2020-10-31 19:38:44 +0100

    [#39] Remove non-existing files from package

--- mix.exs
+++ mix.exs
@@ -32,7 +32,7 @@ defmodule Eden.Mixfile do
   end
 
   defp package do
-    [files: ["lib", "priv", "mix.exs", "README*", "readme*", "LICENSE*", "license*"],
+    [files: ["lib", "mix.exs", "README.md", "LICENSE"],
      contributors: ["Juan Facorro"],
      licenses: ["Apache 2.0"],
      links: %{"GitHub" => "https://github.com/jfacorro/Eden/",
